acaedfa57f5f30bcad6b0f70774fb769f22c53c5
Christy <crizty702@gmail.com>
2026-01-27 23:15:39 +0530

    toggle implementation is done

--- helper.go
+++ helper.go
@@ -34,12 +34,30 @@ func (d *Done) validateIndex(index int) error{
 	return nil
 }
 
-func (d *Done) Delete (index int) error{	
+func (d *Done) Delete(index int) error{	
 	if err := d.validateIndex(index) ; err != nil {
 		return err
 	}
 
 	*d= append((*d)[:index],(*d)[index+1:]...)
+
+	return nil
+}
+
+func (d *Done) toggle(index int) error{
+
+	if err := d.validateIndex(index); err!= nil {
+		return err
+	}
+
+	isCompleted := (*d)[index].Completed
+
+	if !isCompleted{
+		completionTime := time.Now()
+		(*d)[index].CompletedAt = &completionTime
+	}
+
+	(*d)[index].Completed = !isCompleted
 
 	return nil
 }
